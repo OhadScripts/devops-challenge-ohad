3c9f2300e1734f4ba1474345d8f75869fc0c10f2
Ohad Lubaton <ohad@Ohads-MacBook-Pro.local>
2021-01-28 20:38:55 +0200

    mreged all and fixed conflics, working version

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ from SecretController import secret_page
 from HealthController import health_page
 from flask import Flask
 
+app = Flask(__name__)
+app.register_blueprint(secret_page)
+app.register_blueprint(health_page)
+
 
 def main():
-    app = Flask(__name__)
-    app.register_blueprint(secret_page)
-    app.register_blueprint(health_page)
     app.run()
 
 
