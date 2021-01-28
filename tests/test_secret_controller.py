8a97dba58fdfb43d90ec59b7c575c9ec9e3913f1
Ohad Lubaton <ohad@Ohads-MacBook-Pro.local>
2021-01-28 19:39:34 +0200

    added test for secret controller

---
+++ tests/test_secret_controller.py
@@ -0,0 +1,15 @@
+import requests
+
+server_url = "127.0.0.1"
+server_port = "5000"
+
+
+def test_secret_controller():
+    controller_path = "secret"
+    expected_result = ["code_name", "secret_code"]
+
+    result = requests.get(f"http://{server_url}:{server_port}/{controller_path}").json().keys()
+    result = list(result)
+    result.sort()
+
+    assert expected_result != result
